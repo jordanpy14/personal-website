89eb429b7e07c3fe61a28fc27e965903c4647958
jordanperu <jordan14@stanford.edu>
2023-05-26 11:24:13 -0700

    added video in the background

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import PersonalInfo from './PersonalInfo';
 import Story from './Story';
 import Resume from './Resume';
 import Projects from './Projects';
+// import './MarioAnimation.css';
 
 function App() {
   return (
     <div className="App">
+      {/* <div className='mario'></div> */}
+      <video className='background-video' autoPlay loop muted>
+          <source src="videoplayback.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+      </video>
       <PersonalInfo />
       <Story />
-      <Resum />
+      <Resume />
       <Projects />
     </div>
   );

df8bc934cb608d3318c78502ef9d430e5abbfe3e
jordanperu <jordan14@stanford.edu>
2023-05-25 22:34:39 -0700

    Initial Commit

---
+++ src/PersonalInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PersonalInfo = () => {
+    return (
+        <div>
+            <h1>Jordan Paredes</h1>
+            <p>
+                <a href="https://www.linkedin.com/in/jordan-paredes-0a3ab21a6/">LinkedIn</a>
+            </p>
+            <p>
+                <a href="https://github.com/yourname">GitHub</a>
+            </p>
+        </div>
+    );
+};
+
+export default PersonalInfo;

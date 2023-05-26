3fc70d458ec8c34be8a0927666ae538c6b4381fa
jordanperu <jordan14@stanford.edu>
2023-05-25 22:45:06 -0700

    set up

---
+++ src/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <h2>My Projects</h2>
+            <p>
+                <a href="https://www.yourproject1.com/">Project 1</a>
+            </p>
+            <p>
+                <a href="https://www.yourproject2.com/">Project 2</a>
+            </p>
+        </div>
+    );
+};
+
+export default Projects;
---
+++ src/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <h2>My Resume</h2>
+            <p>Your resume goes here...</p>
+        </div>
+    );
+};
+
+export default Resume;
